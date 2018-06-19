302a8eac4334d4b12d96924246364a644f560627
Jashan <jashan.khela@hotmail.com>
2018-06-19 02:10:40 +0000

    char counter is red

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,25 @@
+$(document).ready(function() { console.log('ready') 
+
+$("#tweet-textarea").on('input propertychange paste', function(){ 
+    
+     const $counter = $('.counter');
+     const tweetContent = $(this).val() ;
+     const chartweetCount = Array.from(tweetContent).length ;
+     const remainingChar = 140 - chartweetCount ;
+     console.log(remainingChar);
+     $counter.html(remainingChar) ;
+
+    if(remainingChar < 0) {
+        $('#counter').css('color','red');
+    } else {
+        $('#counter').css('color','black');
+    }
+       })
+
+
+
+
+
+
+});
+
