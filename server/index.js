1b96918094da0b08dceac1843f742ee7c947f1cf
Jashan <jashan.khela@hotmail.com>
2018-06-23 18:18:20 +0000

    update to start implementing login

--- server/index.js
+++ server/index.js
@@ -4,9 +4,13 @@ const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+const cookieSession = require('cookie-session');
+
 const MongoClient = require("mongodb").MongoClient;
+
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 //Open up connection to MongoDB
 MongoClient.connect(MONGODB_URI, (err, db) => {
